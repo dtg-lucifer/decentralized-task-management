09937aaef6720160f8b606068cbaa314d41f7e09
devpiush <dev.bosepiush@gmail.com>
2025-01-09 13:39:27 +0530

    init: configured ganache local network

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { config as dotenvConfig } from "dotenv";
+
+dotenvConfig();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    localganache: {
+      url: process.env.PROVIDER_URL,
+      accounts: [`0x${process.env.ACCOUNT_PRIVATE_KEY}`],
+    },
+  },
 };
 
 export default config;
