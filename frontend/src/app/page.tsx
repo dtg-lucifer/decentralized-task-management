b4eb00f0ae053b0ba601f00c443a9eb05784f1ed
devpiush <dev.bosepiush@gmail.com>
2025-01-09 23:23:52 +0530

    code: added support for dark and light theme

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,11 @@
+import { ThemeToggle } from "@/components/theme/theme-switcher";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1>Hello world</h1>;
+  return (
+    <h1>
+      Hello world<Button>Hello</Button>
+      <ThemeToggle />
+    </h1>
+  );
 }
