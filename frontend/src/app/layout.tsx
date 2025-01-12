bd39a99b3edc04d8796263bb2ae1d853efed71d7
devpiush <dev.bosepiush@gmail.com>
2025-01-12 15:32:28 +0530

    code: added support for dark and light theme

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme/theme-provider";
+import { MetamaskProvider } from "@/hooks/useMetamask";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
+          <MetamaskProvider>{children}</MetamaskProvider>
         </ThemeProvider>
       </body>
     </html>
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,13 @@
 import { ThemeToggle } from "@/components/theme/theme-switcher";
 import { Button } from "@/components/ui/button";
+import { ConnectWalletButton } from "@/components/wallet/connect-wallet";
 
 export default function Home() {
   return (
     <h1>
       Hello world<Button>Hello</Button>
       <ThemeToggle />
+      <ConnectWalletButton />
     </h1>
   );
 }
